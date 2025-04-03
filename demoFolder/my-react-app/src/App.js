405bed456660172d1aa6a72776e133656aa5817d
bobthefish29 <dapunchak@email.neit.edu>
2025-04-02 20:28:58 -0400

    for you

--- demoFolder/my-react-app/src/App.js
+++ demoFolder/my-react-app/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-
+// import Todo from './Todo'
 
 import SignUpForm from './FormExample'
 
@@ -8,6 +8,7 @@ import SignUpForm from './FormExample'
 function App() {
   return (
     
+    // <Todo></Todo>
     <SignUpForm></SignUpForm>
 
   );
